59ebfa2fbe6ab5b717893e91cb7a1f1841ef047d
dewanbatara31 <56387936+dewanbatara31@users.noreply.github.com>
2021-06-11 20:55:32 +0700

    Add files via upload

---
+++ about.php
@@ -0,0 +1,9 @@
+<?php require('header.php'); ?>
+<div class="content">
+    <h2>Ini Halaman Tentang</h2>
+    <p>Tampilan ini adalah bagian content dari halaman.</p>
+</div>
+<footer>
+<p>&copy; 2021, Informatika, Universitas Pelita Bangsa</p>
+</footer>
+© 2021 GitHub, Inc.
